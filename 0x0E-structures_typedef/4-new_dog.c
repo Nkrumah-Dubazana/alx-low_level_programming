7e23833990c8f42ee9e9c00b9bcbbbd18bef80c5
Nkrumah-Dubazana <altonkrumah@gmai.com>
2023-06-20 05:13:23 -0700

    Function that returns the length of a string.

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -5,7 +5,7 @@
  * _strlen - returns the length of a string
  * @s: string to evaluate
  *
- * Returns: the length of the string
+ * Return: the length of the string
  */
 int _strlen(char *s)
 {
@@ -24,7 +24,7 @@ int _strlen(char *s)
 /**
  * *_strcpy - copies the string pointed to by src
  * including the terminating null byte (\0)
- * to the buffer pointed to buy dest
+ * to the buffer pointed to by dest
  * @dest: pointer to the buffer in which we copy the string
  * @src: string to be copied
  *
@@ -40,6 +40,7 @@ char *_strcpy(char *dest, char *src)
 	{
 		len++;
 	}
+
 	for (i = 0; i < len; i++)
 	{
 		dest[i] = src[i];
@@ -88,4 +89,3 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	return (dog);
 }
-
