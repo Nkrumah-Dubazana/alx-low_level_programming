58a71d88db29f77844ac739fadc9a02ee1e2b31d
Nkrumah-Dubazana <altonkrumah@gmai.com>
2023-06-25 10:32:37 -0700

    Function that numbers given paramters.

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,15 @@
+#include "variadic_fuunctions.h"
+
+/**
+ * print_numbers - prints numbers given as parameters
+ * @separator: string to be printed between numbers
+ * @n: number of integers passed to the function
+ */
+void print_numbers(const char *separator, const unsigned int n, ...){
+	unsigned int i;
+	va_list list;
+
+	va_start(list, n);
+
+	for (i = 0; i < n; i++)
+	{
