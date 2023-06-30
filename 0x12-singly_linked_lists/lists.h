b8cfcf185a06d2f5857b363e808558971d08e7c2
Nkrumah-Dubazana <altonkrumah@gmai.com>
2023-06-30 16:02:01 -0700

    Lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,7 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stddef.h>
+
 
 /**
  * struct list_s - singly linked list
@@ -25,5 +25,5 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-#endif /* LISTS_H */
+#endif 
 
