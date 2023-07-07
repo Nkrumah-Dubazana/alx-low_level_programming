afff89515c014dd2ac4da32d953a949aa502b6b8
Nkrumah-Dubazana <altonkrumah@gmai.com>
2023-07-07 15:03:58 -0700

    Function that checks a machine is big endian or little.

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -3,14 +3,14 @@
 /**
  * get_endianness - Checks the endianness of the system.
  *
- * Return: 0 if big endian, 1 if little endian.
+ * Return: 1 if little endian, 0 otherwise.
  */
 int get_endianness(void)
 {
-	int num = 1;
-	char *byte = (char *)&num;
+	unsigned int test = 1;
+	char *endian = (char *)&test;
 
-	if (*byte)
+	if (*endian)
 		return 1; // Little endian
 	else
 		return 0; // Big endian
